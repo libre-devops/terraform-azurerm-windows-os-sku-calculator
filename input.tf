b75a6d1dbeb10a8ec70dc77eefd7092cc6d59e4d
Craig Thacker <craigthackerx@gmail.com>
2022-04-17 23:06:23 +0100

    update os

--- input.tf
+++ input.tf
@@ -2,16 +2,20 @@ variable "vm_os_simple" {
   default = ""
 }
 
-# Definition of the standard OS with "SimpleName" = "publisher,offer,sku"
+# Definition of the standard OS with "SimpleName" = "publisher,offer,sku", this can have many more skus added to it
 variable "standard_os" {
   default = {
-    "UbuntuServer"  = "Canonical,UbuntuServer,20.04-LTS"
-    "WindowsServer" = "MicrosoftWindowsServer,WindowsServer,2019-Datacenter"
-    "RHEL"          = "RedHat,RHEL,8.4"
-    "openSUSE-Leap" = "SUSE,openSUSE-Leap,42.2"
-    "CentOS"        = "OpenLogic,CentOS,7.3"
-    "Debian"        = "credativ,Debian,11"
-    "CoreOS"        = "CoreOS,CoreOS,Stable"
-    "SLES"          = "SUSE,SLES,12-SP2"
+    "WindowsServer2008" = "MicrosoftWindowsServer,WindowsServer,2008-R2-SP1"
+    "WindowsServer2012" = "MicrosoftWindowsServer,WindowsServer,2012-Datacenter"
+    "WindowsServer2016" = "MicrosoftWindowsServer,WindowsServer,2012-Datacenter"
+    "WindowsServer2019" = "MicrosoftWindowsServer,WindowsServer,2019-Datacenter"
+    "WindowsServer2022" = "MicrosoftWindowsServer,WindowsServer,2022-Datacenter"
+
+    "WindowsServerAzureEdition" = "MicrosoftWindowsServer,WindowsServer,2022-datacenter-azure-edition"
+
+    "WindowsServer2019WithContainers" = "MicrosoftWindowsServer,WindowsServer,2019-Datacenter-with-Containers"
+
+    "Windows10" = "MicrosoftWindowsDesktop,Windows-10,win10-21h2-ent-ltsc"
+    "Windows11" = "MicrosoftWindowsDesktop,Windows-11,win11-21h2-ent"
   }
 }
