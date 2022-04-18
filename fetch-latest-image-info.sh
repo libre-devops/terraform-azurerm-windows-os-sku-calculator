fcfa2b51ddd97ce6284fb18959c0100a3163fed1
Craig Thacker <craigthackerx@gmail.com>
2022-04-18 14:51:07 +0100

    update

--- fetch-latest-image-info.sh
+++ fetch-latest-image-info.sh
@@ -3,8 +3,9 @@
 set -xe
 START=$(pwd)
 
-for dir in */ ; do
+# Executes script in each sub directory
+for dir in $(find . -maxdepth 2 -mindepth 1 -type d); do
     cd $dir && \
-    ./get-image-info.sh
+    ./get-image-info.sh || echo "Script not in $dir, skipping"
     cd $START
 done
